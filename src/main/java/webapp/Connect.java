2e13a1c262987148d1df969dda735e8d2373edc8
Josemi <josemijosejo@gmail.com>
2022-11-24 11:25:46 +0530

    first commit

---
+++ src/main/java/webapp/Connect.java
@@ -0,0 +1,23 @@
+package webapp;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+
+public static Connection getConnection() {
+
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:h2:~/test", "root", "root");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+	
+}
